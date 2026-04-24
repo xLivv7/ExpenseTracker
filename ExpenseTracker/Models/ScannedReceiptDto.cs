434d7d4cae280c6d1193caa3f3b1e2acd5684c26
MSI\orypi <orypinska@gmail.com>
2026-04-24 17:03:50 +0200

    feat(Receipt-Scanner): Added DataTransferObject - ScannedReceiptDto.cs

---
+++ ExpenseTracker/Models/ScannedReceiptDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker.Models
+{
+    public class ScannedReceiptDto
+    {
+        public string? MerchantName { get; set; }
+        public decimal? TotalAmount { get; set; }
+        public DateTime? TransactionDate { get; set; }
+        public string? Category { get; set; }
+        public string? SubCategory { get; set; }
+        public string? Description { get; set; }
+    }
+}
